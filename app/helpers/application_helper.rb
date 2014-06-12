e91af289ed543d68ace92781d46491ecff3e3b69
Minh Nguyen <minhn.n@me.com>
2014-06-12 01:19:02 -0400

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	# Returns the full title on a per-page basis.
+	def full_title(page_tilte)
+		base_title = "Chirper"
+		if page_tilte.empty?
+			base_title
+		else
+			"#{base_title}|#{page_tilte}"
+		end
+	end
 end
